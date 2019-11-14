5bf28c6e41471a38d93676b8fc5628a0e708969e
Nick Foden <nicfoden@gmail.com>
2019-11-14 16:40:08 -0500

    Adding font and basic nav

---
+++ styles/theme.js
@@ -0,0 +1,29 @@
+const theme = {
+  font: {
+    sansSerif: '-apple-system, "Helvetica Neue", Arial, sans-serif',
+    mono: "Menlo, Monaco, monospace",
+    Montserrat: "Montserrat, sans-serif"
+  },
+  fontSize: {
+    2: "8px",
+    3: "12px",
+    4: "16px",
+    5: "20px",
+    6: "24px",
+    7: "28px",
+    8: "32px",
+    9: "36px"
+  },
+  colors: {
+    background: "#1f1f1f",
+    navlink: "#fff",
+    text: "#333",
+    link: "#1eaaf1",
+    linkHover: "#0d8ecf",
+    border: "#ddd",
+    warning: "#fff3cd",
+    success: "#d4edda"
+  }
+};
+
+export default theme;
